1840f2c2bc7d1413d4492238614269d6bccecc7e
gacallea <gacallea@users.noreply.github.com>
2021-02-25 19:32:08 +0100

    new leaderlogs

--- roles/local-core/scripts/99_isLeader.sh
+++ roles/local-core/scripts/99_isLeader.sh
@@ -4,27 +4,11 @@ poolId="$1"
 epoch="$2"
 vrfkey="$3"
 timezone="$4"
-next="$5"
-bft="$6"
-sigma=""
-
-function getSigma() {
-    if [ "$next" == "true" ]; then
-        sigma=$(./getSigma.py --pool-id "$poolId" --next | awk '/Sigma/ {print $2}')
-    else
-        sigma=$(./getSigma.py --pool-id "$poolId" | awk '/Sigma/ {print $2}')
-    fi
-}
 
 function getLeader() {
-    if [ "$bft" == "true" ]; then
-        ./leaderLogs.py --pool-id "$poolId" --epoch "$epoch" --vrf-skey "$vrfkey" --tz "$timezone" --sigma "$sigma" -bft
-    else
-        ./leaderLogs.py --pool-id "$poolId" --epoch "$epoch" --vrf-skey "$vrfkey" --tz "$timezone" --sigma "$sigma"
-    fi
+    ./leaderLogs.py --pool-id "$poolId" --epoch "$epoch" --vrf-skey "$vrfkey" --tz "$timezone"
 }
 
-getSigma
 getLeader
 
 exit 0
