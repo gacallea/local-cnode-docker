c45319d923eae3eef4797873c77ffde966a97ecb
gacallea <gacallea@users.noreply.github.com>
2021-09-24 16:41:17 +0200

    fixed url

--- roles/local-core/scripts/get_files.sh
+++ roles/local-core/scripts/get_files.sh
@@ -3,7 +3,8 @@
 DOWNLOAD_DIR="$1"
 BUILD="$2"
 VERSION="$3"
-CNODE_TAR="https://hydra.iohk.io/build/{$BUILD}/download/1/cardano-node-${VERSION}-linux.tar.gz"
+FETCH_URL="https://hydra.iohk.io/build/${BUILD}/download/1/cardano-node-${VERSION}-linux.tar.gz"
+CNODE_TAR="cardano-node-${VERSION}-linux.tar.gz"
 
 if ! command -v curl >/dev/null; then
     echo "cURL not found. please install curl!"
@@ -16,7 +17,7 @@ if ! command -v lynx >/dev/null; then
 fi
 
 if [[ ! -f "$DOWNLOAD_DIR"/"$CNODE_TAR" ]]; then
-    curl -sLJ "$CNODE_TAR" -o "$DOWNLOAD_DIR"/"$CNODE_TAR"
+    curl -sLJ "$FETCH_URL" -o "$DOWNLOAD_DIR"/"$CNODE_TAR"
     tar xzvf "$DOWNLOAD_DIR"/"$CNODE_TAR" -C "$DOWNLOAD_DIR/"
 else
     echo "you already have downloaded the latest cardano-node"
